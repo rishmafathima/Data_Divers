64fe677d58f7689c25fc1681e57c7c947f60fe85
Mikhel Adam <mikhel_adam@hotmail.com>
2023-06-28 13:16:54 +0800

    update

---
+++ .history/userlogin/routes/web_20230628131359.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+ 
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+
+    Route::get('/edit-profile', [AuthController::class, 'editProfile'])->name('edit-profile');
+    Route::post('/update-profile', [AuthController::class, 'updateProfile'])->name('update-profile');
+    Route::post('/delete-profile', [AuthController::class, 'deleteProfile'])->name('delete-profile');
+
+    
+    Route::get('/home', [UserController::class, 'Show'])->name('home');
+});
